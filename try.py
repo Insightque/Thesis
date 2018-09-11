ebb2cf47b1e0e5794e506bc18d34497fa4b995a3
paszti96 <paszti96@gmail.com>
2018-09-11 17:19:26 +0200

    Reach the goal
    Don't know why it does not work with f.

--- try.py
+++ try.py
@@ -3,13 +3,13 @@ import math
 
 SIZEX = 1000
 SIZEY = 700
-BORDER = 100
+BORDER = 50
 RADIUS = (BORDER / 2) + 1
 WINDOW = graphics.GraphWin("Pathfind", SIZEX, SIZEY)
 CIRCLES_2_DRAW = []
 pi = 3.14
 # SAMPLE = 5
-SAMPLE = 2
+SAMPLE = 5
 
 def calc_distance(node1, node2):
     """calculating Eucledian distance from a node to an other"""
@@ -31,7 +31,7 @@ class Node:
     h = 0
     f = 0
 
-    def set_distance(self, reference):
+    def calc_cost(self, reference):
         # dist2 = (self.parent.x - self.x)**2 + (self.parent.y - self.y)**2
         # self.g = math.sqrt(dist2)
         if self.parent is not None:
@@ -67,14 +67,30 @@ class Search:
 
     S_open = []
     S_closed = []
+    NODES = []
+
+    def __init__(self, Start, Goal):
+        # define the start circle
+        self.n_start = Start
+        self.n_start.circle.draw_circle().setOutline("green")
+        self.S_open.append(self.n_start)
+        self.NODES.append(self.n_start)
+
+        # define the goal circle
+        self.n_goal = Goal
+        self.n_goal.circle.draw_circle().setOutline("red")
+
+        self.n_start.calc_cost(self.n_goal)
+        self.n_goal.calc_cost(self.n_start)                  # TODO: a cél távolságának becslésére ez jó?
+        self.n_goal.f *= 3
 
     def expand(self, node):
         global CIRCLES_2_DRAW
         for i in range(0, SAMPLE):
-            alpha = (3.14/2) * (i/4)
+            alpha = (3.14 / 2) * (i / (SAMPLE - 1))
             dy = node.circle.r * math.sin(alpha)
             dx = node.circle.r * math.cos(alpha)
-            
+
             node_new1 = Node(node.circle.x + dx, node.circle.y + dy)
             node_new1.direction = Vector(dx, dy)
 
@@ -88,69 +104,85 @@ class Search:
             node_new4.direction = Vector(-dx, -dy)
 
             new_nodes = [node_new1, node_new2, node_new3, node_new4]
-            for n in new_nodes:
+            for index, n in reversed(list(enumerate(new_nodes))):
                 n.parent = node
-                n.set_distance(self.n_goal)
-                for index, element in enumerate(self.S_open,0):
-                    if element.x == n.x and element.y == n.y and element.direction == n.direction and (calc_distance(n, element) <= n.r):              # TODO: nem tudom hogy itt kell e gyorsítani de próbának jó lesz
-                        self.S_open.pop(index)
+                n.calc_cost(self.n_goal)
+                for element in self.NODES:
+                    if element.x == n.x and element.y == n.y and element.direction == n.direction:  # TODO: nem tudom hogy itt kell e gyorsítani de próbának jó lesz
+                        new_nodes.pop(index)
+                        break
+                    elif calc_distance(element, n) < n.circle.r:
+                        new_nodes.pop(index)
+                        break
             self.S_open += new_nodes
-
-
+            CIRCLES_2_DRAW += new_nodes
+            self.NODES += new_nodes
 
         # drawing the circles to the window
-        CIRCLES_2_DRAW += new_nodes
-        for index,node in enumerate(CIRCLES_2_DRAW,0):
+
+        for index, node in reversed(list(enumerate(CIRCLES_2_DRAW))):
             node.circle.draw_circle().setOutline("purple")
             CIRCLES_2_DRAW.pop(index)
 
-    def __init__(self, Start, Goal):
-        # define the start circle
-        self.n_start = Start
-        self.n_start.circle.draw_circle().setOutline("green")
-        self.S_open.append(self.n_start)
-
-        # define the goal circle
-        self.n_goal = Goal
-        self.n_goal.circle.draw_circle().setOutline("red")
-
-        self.n_start.set_distance(self.n_goal)
-        self.n_goal.set_distance(self.n_start)                  # TODO: a cél távolságának becslésére ez jó?
-
     def not_exist(self, node):
-        """Checking if there is an existing, redundant circle"""
-        return True
+        not_find = True
+        for finished in self.S_closed:
+            if node == finished:
+                not_find = False
+                break
+        return not_find
 
-    def PopNearest(self):
+    def PopNearest(self):                       # TODO: Itt mit is keresünk? f-et? az így nagyon elment?
         """pop from the S_open the nearest node to the goal, to be evaluated"""
         # TODO: Geci lassu
         min_index = 0
-        min_dist = self.S_open[0].f
+        min_dist = self.S_open[0].h
         for index, node in enumerate(self.S_open,0):
-            if node.f < min_dist:
+            if node.h < min_dist:
                 min_index = index
-                min_dist = node.f
+                min_dist = node.h
         return self.S_open.pop(min_index)
 
+    def overlap(self,node1,node2):
+        if calc_distance(node1, node2) <= (node1.circle.r + node2.circle.r):
+            return True
+        else:
+            return False
+
+    def recurse(self):
+        node = self.n_goal
+        while node != self.n_start:
+            parent = node.parent
+            pt1 =graphics.Point(parent.x,parent.y)
+            pt2 = graphics.Point(node.x,node.y)
+            ln = graphics.Line(pt1, pt2)
+            ln.setWidth(10)
+            ln.setOutline('orange')
+            ln.draw(WINDOW)
+            node = parent
+
     def space_exploration(self):
-        self.expand(self.n_start)
-
-
        # TODO: here to start
         while self.S_open:
             node = self.PopNearest()
-            if calc_distance(self.n_goal, node) < self.n_goal.circle.r:          # TODO: valahogy ellenőrizzük ha ott vagyunk a célnál IDK if it's good
+            if self.n_goal.parent is not None:          # TODO: valahogy ellenőrizzük ha ott vagyunk a célnál IDK if it's good
+                self.recurse()
                 return True
 
             elif self.not_exist(node):
                 self.expand(node)
-                # TODO: folytasd....
+                if self.overlap(node, self.n_goal):
+                    self.n_goal.g = node.f
+                    self.n_goal.parent = node
+                self.S_closed.append(node)
+        return False
 
 def main():
     start = Node(BORDER, BORDER)
-    goal = Node(SIZEX, SIZEY)
+    goal = Node(SIZEX-BORDER, SIZEY-BORDER)
     test = Search(start, goal)
-    test.space_exploration()
+    if test.space_exploration():
+        print("DONE")
     while True:
         """Waiting for click and closing the window"""
         try:
